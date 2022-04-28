dd781c8a8d5e0e043b16f8e401bdbe27325e72fa
Vivek Mani Charan P <cs19b032@iittp.ac.in>
2022-04-28 11:50:18 +0530

    modified procfile

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from chat import get_response, bot_name
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello():
-    return 'Hello'
+def helloworld():
+    return 'Hello World'
 
 # if __name__ == "__main__":
 #     msg = input("Enter Msg to Chatbot")
